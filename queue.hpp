633741d57d9493feab4337c68cdf71f01f9f23cf
Sama Ahmed <111910374+26samaahmed@users.noreply.github.com>
2023-10-10 01:10:58 +0000

    create first implementation (wip)

--- queue.hpp
+++ queue.hpp
@@ -11,22 +11,34 @@ public:
 
     void push(T value)
     {
+        if (container_.size() == container_.capacity()) {
+            container_.push(value);
+        } else {
+            auto i = (container_.front() + container_.size_()) % container_.capacity();
+            container_[i] = value;
+            ++size_;
+        }
+        
     }
 
     void pop()
     {
+        container_.pop();
     }
 
     // 💡TIP: in this implementation lets return the value so that we can print it in main.cpp
     T front()
     {
+        
     }
 
     T back()
     {
+        return container_.back();
     }
 
 private:
     std::vector<T> container_;
     std::size_t size_;
+    std::size_t front_;
 };
